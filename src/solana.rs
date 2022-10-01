26443ab2aaac2df8baa7a6bbd760e4bc2fc77f1c
Christian M <chriamue@gmail.com>
2022-10-01 17:09:50 +0200

    show balance of solana wallet

--- src/solana.rs
+++ src/solana.rs
@@ -9,7 +9,9 @@ pub fn mint(_pubkey: String) {
     dotenv().ok();
     let solana_key = env::var("SOLANA_KEY").expect("SOLANA_KEY must be set");
     let rpc_client = RpcClient::new("https://api.devnet.solana.com");
+
     let keypair = Keypair::from_base58_string(&solana_key);
+    rpc_client.request_airdrop(&keypair.pubkey(), 1).unwrap();
     println!("pubkey: {}", keypair.pubkey().to_string());
     let balance = rpc_client.get_balance(&keypair.pubkey());
     println!("balance: {}", balance.unwrap());
